80d4f2908a56c129e16a353dd3179b065da14ec9
Re-he4i5 <kuronekoryohei@gmail.com>
2022-08-05 10:02:42 +0000

    [add]controller_view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @book_new = Book.new
@@ -7,16 +8,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book_new = Book.new(book_params)
     @book_new.user_id = current_user.id
     if @book_new.save
-      flash[:notice] = "successfully"
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book_new.id)
     else
       @books = Book.all
@@ -26,15 +31,31 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
-  get 'homes/about' =>'homes#about' ,as: 'about'
+  get 'home/about' =>'homes#about' ,as: 'about'
 
   resources :books
   resources :users
