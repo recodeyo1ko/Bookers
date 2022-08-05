cdb889616dba369309b6b60fe06d315ae88dc5cb
Re-he4i5 <kuronekoryohei@gmail.com>
2022-08-05 09:10:41 +0000

    [add]create_book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
 
   def index
+    @book_new = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
@@ -10,12 +13,28 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book_new.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
