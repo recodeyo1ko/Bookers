643153c90d6fe45a22a13c0bf004058cf131c629
Re-he4i5 <kuronekoryohei@gmail.com>
2022-08-07 08:23:55 +0000

    [finish]relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,20 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :follower, source: :followed
+  has_many :followers, through: :followed, source: :follower
+
+  def follow(user_id)
+  follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
   validates :name, uniqueness: true, length: {in: 2..20}
   validates :introduction, length: {maximum: 50}
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users
+
+  resources :users do
+    resource :relationships, only: [:create,:destroy]
+     get 'followings' => 'relationships#followings', as: 'followings'
+     get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 
 end
