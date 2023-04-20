0d5b688f284d8001179956346051f68b4b3f7666
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-04-20 20:50:37 +0900

    検索機能が邪魔だったので、一覧ページのみに適応

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,11 +1,19 @@
 class ChatsController < ApplicationController
     def show
         @user = User.find(params[:id])
+        
     end
 
     def create
-        
+        @chat = current_user.chats.new(chat_params)
+        @room = @chat.room
+        @chats = @room.chats
+        # if @chat.save
+        #     redirect_to 
     end
     private
+    def chat_params
+        params.require(:chat).permit(:message, :room_id)
+    end
     
 end
