0c9ac5ba3db1c9aa4c8a05b9576aff99862efb27
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-05-25 15:37:11 +0900

    [add]group create & index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,35 @@
 class GroupsController < ApplicationController
 
-  def index
+  def new
+    @group = Group.new
   end
-
-  def create
+  
+  def index
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(:id)
+  end
+
+  def edit
+    @group = Group.find(:id)
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save!
+    redirect_to groups_path
   end
 
   def update
+    @group = Group.find(:id)
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name , :introduction , :image)
   end
 
 end
