a5e0422e7d749bf05f03e8135ce4d7fcdf75d9d7
Re-he4i5 <kuronekoryohei@gmail.com>
2023-05-26 22:58:43 +0900

    [add]ownerのパラメータの許可

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,14 +6,16 @@ class GroupsController < ApplicationController
   
   def index
     @groups = Group.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
-    @group = Group.find(:id)
+    @group = Group.find(params[:id])
   end
 
   def edit
-    @group = Group.find(:id)
+    @group = Group.find(params[:id])
   end
 
   def create
@@ -23,13 +25,13 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(:id)
+    @group = Group.find(params[:id])
   end
 
   private
 
   def group_params
-    params.require(:group).permit(:name , :introduction , :image)
+    params.require(:group).permit(:name , :introduction , :image, :owner_id)
   end
 
 end
