f3d5fc96ad01c8552535df67ff5b9e93593ffbef
Re-he4i5 <kuronekoryohei@gmail.com>
2023-06-07 22:46:58 +0900

    [fix]update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,6 +29,9 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to group_path(@group.id)
+
   end
 
   def join
