0b053832a775e1e1a4532947a296221875e90fb6
Re-he4i5 <kuronekoryohei@gmail.com>
2023-06-24 02:04:26 +0900

    [finish]7d

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application"
-import '@fortawesome/fontawesome-free/js/all'
+import "../stylesheets/application";
+import "@fortawesome/fontawesome-free/js/all";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import Raty from "raty.js";
+window.raty = function (elem, opt) {
+  let raty = new Raty(elem, opt);
+  raty.init();
+  return raty;
+};
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_08_05_082511) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.float "star"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
