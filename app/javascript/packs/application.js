7c923fc511bf0598dcad648ff30b0cf81a234dd4
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-09-06 15:23:46 +0900

    住所登録機能、[gem]jp_prefecture、jpostalの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,18 @@ window.raty = function (elem, opt) {
 import Chart from "chart.js/auto";
 global.Chart = Chart;
 
-
 Rails.start();
 // はずしたほうがいいかも
 Turbolinks.start();
 ActiveStorage.start();
+
+jQuery(document).on("turbolinks:load", function () {
+  $("#user_postalcode").jpostal({
+    postcode: ["#user_postalcode"],
+    address: {
+      "#user_prefecture_code": "%3",
+      "#user_address_city": "%4%5",
+      "#user_address_street": "%6%7",
+    },
+  });
+});
--- db/migrate/20220805080538_devise_create_users.rb
+++ db/migrate/20220805080538_devise_create_users.rb
@@ -8,6 +8,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ""
       t.string :name
       t.text :introduction
+      t.integer :postalcode
+      t.integer :prefecture_code
+      t.string :address_city
+      t.string :address_street
+      t.string :address_building
 
       ## Recoverable
       t.string   :reset_password_token
